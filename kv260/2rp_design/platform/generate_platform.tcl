9ff9618dba482273d454fd56942bab205646f7d4
Deeksha Nandal <deeksha.nandal@xilinx.com>
2022-02-21 19:02:02 +0530

    Update generate_platform.tcl

--- kv260/2rp_design/platform/generate_platform.tcl
+++ kv260/2rp_design/platform/generate_platform.tcl
@@ -4,5 +4,5 @@ set HW_XSA [dict get $options HW_XSA]
 set platform_out [dict get $options platform_out]
 
 platform create -name {opendfx_kv260} -desc " A 2rp dfx platform targeting KV260 exported to vitis" -hw $HW_XSA -out $platform_out -no-boot-bsp
-domain create -name xrt -proc psv_cortexa72 -os linux 
+domain create -name xrt -proc psu_cortexa53 -os linux 
 platform generate
