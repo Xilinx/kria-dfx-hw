338026c8a085de78f79e8265737b3d1daf0808b3
deekshad <deekshad@xilinx.com>
2022-03-11 00:31:06 -0800

    aes192 test

---
+++ k26_testcases/Standalone/testAES192.tcl
@@ -0,0 +1,35 @@
+#Update the path for variable VITIS_PATH in environment.tcl
+connect
+source ${VITIS_PATH}/2022.1/scripts/vitis/util/zynqmp_utils.tcl
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+
+puts "\nLoading bit files ..."
+fpga -no-revision-check -file ${SHELLBASE}
+fpga -no-revision-check -file ${AES192_0}
+fpga -no-revision-check -file ${AES192_1}
+
+puts "done"
+targets -set -nocase -filter {name =~"APU*"}
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+set mode [expr [mrd -value 0xFF5E0200] & 0xf]
+targets -set -nocase -filter {name =~ "*A53*#0"}
+rst -processor
+dow ${FSBL}
+
+set bp_20_54_fsbl_bp [bpadd -addr &XFsbl_Exit]
+con -block -timeout 60
+bpremove $bp_20_54_fsbl_bp
+
+puts "Loading data files ..."
+dow -data -force ${AES192_IN_DATA}  0x02000000
+dow -data -force ${AES192_OUT_DATA} 0x04000000
+
+targets -set -nocase -filter {name =~ "*A53*#0"}
+rst -processor
+dow ${AES192_ELF}
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A53*#0"}
+con
