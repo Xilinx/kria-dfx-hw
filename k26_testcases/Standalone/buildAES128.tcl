928bb7920dd42048581e7ade69596cd9a8d1b453
deekshad <deekshad@xilinx.com>
2022-04-14 09:22:30 -0700

    build tcl

--- k26_testcases/Standalone/buildAES128.tcl
+++ k26_testcases/Standalone/buildAES128.tcl
@@ -10,28 +10,28 @@ set APP "AES128"
 setws ${WS}
 if [file exists ${WS}/${XSA}_platform_0] {
 	puts "Platform exists."
-	puts "Activating Platform ...."
-	platform active ${XSA}_platform_0
+		puts "Activating Platform ...."
+		platform active ${XSA}_platform_0
 } else {
 	puts "Creating Platform ....."
-	platform create -name ${XSA}_platform_0 -hw ${PATH}/${XSA}.xsa
+		platform create -name ${XSA}_platform_0 -hw ${PATH}/${XSA}.xsa
 }
 platform report
 domain list
 if {![file exists ${WS}/${XSA}_platform_0/psu_cortexa53_0/${DOMAIN}]} {
 	puts "Creating Domain ....."
-	domain create -name ${DOMAIN} -os standalone -proc psu_cortexa53_0
-	platform generate
+		domain create -name ${DOMAIN} -os standalone -proc psu_cortexa53_0
+		platform generate
 } else {
 	puts "Domain exists."
-	domain active ${DOMAIN}
+		domain active ${DOMAIN}
 }
 repo -libs
 
 if {![file exists ${WS}/${APP}]} {
 	puts "Creating APP ....."
-	app create -name ${APP} -domain ${DOMAIN} -template "Empty Application(C)"
-	importsources -name ${APP} -linker-script -path ${SRCM}
+		app create -name ${APP} -domain ${DOMAIN} -template "Empty Application(C)"
+		importsources -name ${APP} -linker-script -path ${SRCM}
 	importsources -name ${APP} -path ${SRC}
 } else {
 	puts "APP exists."
