d533b283ab528b42e6dbb96203c6de0ac10c1c94
deekshad <deekshad@xilinx.com>
2022-04-14 09:46:57 -0700

    build tcl

--- k26_testcases/linux/buildFIR.tcl
+++ k26_testcases/linux/buildFIR.tcl
@@ -1,36 +1,35 @@
 set XSA "opendfx_shell_wrapper"
-#Add the XSA path 
 set PATH "../../k26/2rp_design/project_1"
-set SRC "./AES128"
-set WS "workspace_AES128"
+set SRC "./FIR"
+set WS "workspace_FIR"
 set SYSCFG "app_sysconfig"
 set DOMAIN "app_domain"
-set APP "AES128"
+set APP "FIR"
 setws ${WS}
 if [file exists ${WS}/${XSA}_platform_0] {
 	puts "Platform exists."
-		puts "Activating Platform ...."
-		platform active ${XSA}_platform_0
+	puts "Activating Platform ...."
+	platform active ${XSA}_platform_0
 } else {
 	puts "Creating Platform ....."
-		platform create -name ${XSA}_platform_0 -hw ${PATH}/${XSA}.xsa
+	platform create -name ${XSA}_platform_0 -hw ${PATH}/${XSA}.xsa
 }
 platform report
 domain list
 if {![file exists ${WS}/${XSA}_platform_0/psu_cortexa53_0/${DOMAIN}]} {
 	puts "Creating Domain ....."
-		domain create -name ${DOMAIN} -os linux -proc psu_cortexa53_0
-		platform generate
+	domain create -name ${DOMAIN} -os linux -proc psu_cortexa53_0
+	platform generate
 } else {
 	puts "Domain exists."
-		domain active ${DOMAIN}
+	domain active ${DOMAIN}
 }
 repo -libs
 
 if {![file exists ${WS}/${APP}]} {
 	puts "Creating APP ....."
-		app create -name ${APP} -domain ${DOMAIN} -template "Empty Application(C)"
-		importsources -name ${APP} -linker-script -path ${SRC}
+	app create -name ${APP} -domain ${DOMAIN} -template "Empty Application(C)"
+	importsources -name ${APP} -linker-script -path ${SRC}
 } else {
 	puts "APP exists."
 
