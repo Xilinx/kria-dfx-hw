defe79f0fa1cfb2e7ed71c7ef274e12b80590763
Deeksha Nandal <deeksha.nandal@xilinx.com>
2022-04-16 23:43:36 +0530

    Update generate_platform.tcl

--- k26/2rp_design/platform/generate_platform.tcl
+++ k26/2rp_design/platform/generate_platform.tcl
@@ -3,6 +3,6 @@ set options [dict create {*}$argv]
 set HW_XSA [dict get $options HW_XSA]
 set platform_out [dict get $options platform_out]
 
-platform create -name {opendfx_k26_platform} -desc " A 2rp dfx platform targeting K26 exported to vitis" -hw $HW_XSA -out $platform_out -no-boot-bsp
+platform create -name {opendfx_shell_wrapper_platform_0} -desc " A 2rp dfx platform targeting K26 exported to vitis" -hw $HW_XSA -out $platform_out -no-boot-bsp
 domain create -name xrt -proc psu_cortexa53 -os linux 
 platform generate
