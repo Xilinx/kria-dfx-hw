8ee9c36168b17a1c3859d751adf9b6af510a6366
deekshad <deekshad@xilinx.com>
2022-06-28 02:11:02 -0700

    pp_pipeline rm tcl

--- k26/2rp_design/rm_tcl/pp_pipeline.tcl
+++ k26/2rp_design/rm_tcl/pp_pipeline.tcl
@@ -285,38 +285,17 @@ proc cr_bd_pp_pipeline { parentCell designName} {
   connect_bd_net -net xlconstant_1_dout [get_bd_pins xlconcat_0/In1] [get_bd_pins xlconcat_0/In2] [get_bd_pins xlconcat_0/In3] [get_bd_pins xlconstant_1/dout]
 
   # Create address segments
-  assign_bd_address -offset 0x00000000 -range 0x80000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
-  assign_bd_address -offset 0xC0000000 -range 0x20000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg_1] -force
-  assign_bd_address -offset 0xFF000000 -range 0x01000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg_2] -force
-  assign_bd_address -offset 0x000200000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg_3] -force
-  assign_bd_address -offset 0x000280000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg_4] -force
-  assign_bd_address -offset 0x000800000000 -range 0x000800000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg_5] -force
-  assign_bd_address -offset 0x00000000 -range 0x80000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
-  assign_bd_address -offset 0xC0000000 -range 0x20000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg_1] -force
-  assign_bd_address -offset 0xFF000000 -range 0x01000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg_2] -force
-  assign_bd_address -offset 0x000200000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg_3] -force
-  assign_bd_address -offset 0x000280000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg_4] -force
-  assign_bd_address -offset 0x000800000000 -range 0x000800000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg_5] -force
-  assign_bd_address -offset 0x00000000 -range 0x80000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
-  assign_bd_address -offset 0xC0000000 -range 0x20000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg_1] -force
-  assign_bd_address -offset 0xFF000000 -range 0x01000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg_2] -force
-  assign_bd_address -offset 0x000200000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg_3] -force
-  assign_bd_address -offset 0x000280000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg_4] -force
-  assign_bd_address -offset 0x000800000000 -range 0x000800000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg_5] -force
-  assign_bd_address -offset 0x00000000 -range 0x80000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
-  assign_bd_address -offset 0xC0000000 -range 0x20000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg_1] -force
-  assign_bd_address -offset 0xFF000000 -range 0x01000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg_2] -force
-  assign_bd_address -offset 0x000200000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg_3] -force
-  assign_bd_address -offset 0x000280000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg_4] -force
-  assign_bd_address -offset 0x000800000000 -range 0x000800000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg_5] -force
+  assign_bd_address -offset 0x0002000000000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
+  assign_bd_address -offset 0x0002000000000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
+  assign_bd_address -offset 0x0002000000000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
+  assign_bd_address -offset 0x0002000000000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs M_AXI_GMEM/Reg] -force
   assign_bd_address -offset 0x80000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces S_AXI_CTRL] [get_bd_addr_segs pp_pipeline_accel_0/s_axi_control/Reg] -force
 
   # Exclude Address Segments
-  exclude_bd_addr_seg -offset 0x0002000000000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
-  exclude_bd_addr_seg -offset 0x0002000000000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
-  exclude_bd_addr_seg -offset 0x0002000000000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
-  exclude_bd_addr_seg -offset 0x0002000000000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
-
+  exclude_bd_addr_seg -offset 0x00000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem1] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
+  exclude_bd_addr_seg -offset 0x00000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem2] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
+  exclude_bd_addr_seg -offset 0x00000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem3] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
+  exclude_bd_addr_seg -offset 0x00000000 -range 0x0002000000000000 -target_address_space [get_bd_addr_spaces pp_pipeline_accel_0/Data_m_axi_gmem4] [get_bd_addr_segs rm_comm_box_0/s_axi_accel/reg0]
 
   # Restore current instance
   current_bd_instance $oldCurInst
