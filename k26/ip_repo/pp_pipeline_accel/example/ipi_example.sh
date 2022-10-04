a0136bfc926befab0d12cdc6373375457c98a76c
vijc <vijc@xilinx.com>
2022-10-04 15:09:32 +0530

    Remove unwanted files

--- k26/ip_repo/pp_pipeline_accel/example/ipi_example.sh
+++
@@ -1,4 +0,0 @@
-# ==============================================================
-# ==============================================================
-
-/proj/xbuilds/SWIP/2022.1_0420_0327/installs/lin64/Vivado/2022.1/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xck26-sfvc784-2LV-c ../xilinx_com_hls_pp_pipeline_accel_1_0.zip
